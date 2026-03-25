b27da4ec68cb9bfefb32b3a33413d452731a797d
João Grimaldi <jggrimaldi12@gmail.com>
2026-03-25 20:13:14 -0300

    feat: user model

---
+++ apps/backend/src/main/java/com/limpac/backend/entity/User.java
@@ -0,0 +1,94 @@
+package com.limpac.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotBlank
+    @Column(name = "full_name")
+    private String fullName;
+
+    @NotBlank
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Column(name = "phone")
+    private String phone;
+
+    public User(UUID id, String fullName, String cnpj, String email, String phone) {
+        this.id = id;
+        this.fullName = fullName;
+        this.cnpj = cnpj;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
