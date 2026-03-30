e944f4498803c8a21448c18eb520004184f0ccd7
João Grimaldi <jggrimaldi12@gmail.com>
2026-03-30 11:54:07 -0300

    refactor: matching dto whith entity

--- apps/backend/src/main/java/com/limpac/backend/dto/CalculationResponseDTO.java
+++ apps/backend/src/main/java/com/limpac/backend/dto/CalculationResponseDTO.java
@@ -7,17 +7,26 @@ import java.util.UUID;
 
 public record CalculationResponseDTO(
         UUID id,
-        Double transactionVolume,
-        Double savedCo2,
-        Double savedPlastic,
-        Double savedPaper,
-        LocalDateTime dateCreate,
-        Integer equivalentTrees,
-        LocalDateTime DateCreate
-       // String ManagerName
+        Double cardVolume,
+        Double co2Saved,
+        Double plasticSaved,
+        Double paperSaved,
+        Double physicalCo2,
+        Double digitalCo2,
+        Integer treeEquivalents,
+        LocalDateTime createdAt
+        // String ManagerName
 ) {
-
-    public CalculationResponseDTO(UUID id, Double transactionVolume, Double savedCo2, Double savedPlastic, Double savedPaper, Integer equivalentTrees, LocalDateTime dateCreate) {
-        this(id, transactionVolume, savedCo2, savedPlastic, savedPaper, dateCreate, equivalentTrees, dateCreate);
+    public CalculationResponseDTO(UUID id, Double cardVolume, Double co2Saved, Double plasticSaved, Double paperSaved, Double physicalCo2, Double digitalCo2, Integer treeEquivalents, LocalDateTime createdAt) {
+        this.id = id;
+        this.cardVolume = cardVolume;
+        this.co2Saved = co2Saved;
+        this.plasticSaved = plasticSaved;
+        this.paperSaved = paperSaved;
+        this.physicalCo2 = physicalCo2;
+        this.digitalCo2 = digitalCo2;
+        this.treeEquivalents = treeEquivalents;
+        this.createdAt = createdAt;
     }
 }
+
